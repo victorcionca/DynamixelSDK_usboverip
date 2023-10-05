c280152054fe81738f3c702e12d12f09cf0c50c1
Victor Cionca <victorcionca@gmail.com>
2023-10-05 18:27:23 +0100

    Fixed bug in IP port handler to return proper length when writing

--- dynamixel_sdk/src/dynamixel_sdk/port_handler_ip.cpp
+++ dynamixel_sdk/src/dynamixel_sdk/port_handler_ip.cpp
@@ -195,15 +195,13 @@ int PortHandlerIP::writePort(uint8_t *packet, int length)
   int result = 0;
   uint16_t new_length = htons(length);
   uint8_t *buf = (uint8_t*)malloc(length+2+2);
-  printf("[PortHandlerIP:write] send %d bytes\n", length);
   buf[0] = PORT_HANDLER_IP_PKTSTART;
-  printf("[PortHandlerIP:write] newlength=%02X\n", new_length);
   ((uint16_t*)(buf+1))[0] = new_length;
   memcpy(buf+3, packet, length);
   buf[3+length] = PORT_HANDLER_IP_PKTEND;
-  result = write(socket_fd_, buf, length+2+2);
+  result = send(socket_fd_, buf, length+2+2, 0);
   free(buf);
-  return result;
+  return result-4; // account for 4 framing bytes
 }
 
 void PortHandlerIP::setPacketTimeout(uint16_t packet_length)
